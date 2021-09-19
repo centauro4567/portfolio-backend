2d0aa7efc17fe54aad1a2128218bb926146e71aa
ladis <ladislao_m_a@hotmail.com>
2021-09-19 20:55:00 -0300

    modificacion del model

--- models/proyecto.js
+++ models/proyecto.js
@@ -5,7 +5,8 @@ const proyectSchema = new schema({
     name: {type:String, required:true},
     description: {type:String, required:true},
     langs: {type:String, required:true},
-    imgPath: {type:String, required:true}
+    imgPath: {type:String, required:true},
+    real: {type:Boolean, default:false}
 })
 
 module.exports = mongoose.model('proyect', proyectSchema);
